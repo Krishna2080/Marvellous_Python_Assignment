b5691b7d0dff44909f0d8293bb9b08deeec03c3a
Padmanabh Madaswar <114794086+Krishna2080@users.noreply.github.com>
2025-05-18 00:33:52 +0530

    Update Assignment2_9.py

--- Assignment_2/Assignment2_9.py
+++ Assignment_2/Assignment2_9.py
@@ -1,8 +1,7 @@
+#9. Write a program which accept number from user and return number of digits in that number. 
 def main():
     a=input("enter number : ")
     print("length is : ",len(a))
-       
-   
-
+    
 if __name__=="__main__":
     main()
