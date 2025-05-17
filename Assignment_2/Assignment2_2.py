8205958ddf1024ad1390697372cad9121faac875
Padmanabh Madaswar <114794086+Krishna2080@users.noreply.github.com>
2025-05-18 00:28:27 +0530

    Update Assignment2_2.py

--- Assignment_2/Assignment2_2.py
+++ Assignment_2/Assignment2_2.py
@@ -1,8 +1,13 @@
+#2. Write a program which accept one number and display below pattern. 
+#       *    *    *    *    *
+#       *    *    *    *    *
+#       *    *    *    *    *
+#       *    *    *    *    *
+#       *    *    *    *    *
 def main():
     no1=int(input("enter number"))
     for i in range(no1):
      print("* \t"*no1)
-     
-
+        
 if __name__=="__main__":
     main()
