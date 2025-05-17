93fad497b81781e800dd1cf8c596034e9bb6f02a
Padmanabh Madaswar <114794086+Krishna2080@users.noreply.github.com>
2025-05-17 16:22:25 +0530

    Add files via upload

--- Assignment_3/Assignment3_1.py
+++ Assignment_3/Assignment3_1.py
@@ -1,1 +1,22 @@
+from functools import reduce
+def Sum (A,B):
+    return A+B
 
+def main():
+    a=int(input("how many number you want to store : "))
+    b=list()
+    for i in range(a):
+        no=int(input())
+        b.append(no)
+
+    result=reduce(Sum,b)
+    print("your list addition is : ",result)
+       
+if __name__=="__main__":
+    main()
+
+
+#result=0
+    #for i in b:
+       #result=result+i
+    
