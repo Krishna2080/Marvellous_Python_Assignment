79da5952135d06058d98208be388d66c5f9b017a
Padmanabh Madaswar <114794086+Krishna2080@users.noreply.github.com>
2025-05-18 00:24:31 +0530

    Update Assignment2_1.py

--- Assignment_2/Assignment2_1.py
+++ Assignment_2/Assignment2_1.py
@@ -1,6 +1,9 @@
-from Arithmetic import Add,Sub,Mult,Div
-def main():
+#1.Create on module named as Arithmetic which contains 4 functions as Add() for addition, Sub()
+#for subtraction, Mult() for multiplication and Div() for division. All functions accepts two parameters
+#as number and perform the operation. Write on python program which call all the functions from Arithmetic module
+#by accepting the parameters from user. from Arithmetic import Add,Sub,Mult,Div
 
+def main():
     a1=float(input("enter first number : "))
     a2=float(input("enter 2nd number : "))
 
@@ -12,10 +15,6 @@ def main():
     print("Your Multiplication : ",c)
     d=Div(a1,a2)
     print("Your division is : ",d)
-
-    
-
-
 
 if __name__=="__main__":
     main()
