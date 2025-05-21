62149a1a023f07eacb73ad78c80fd905f869c6a8
Padmanabh Madaswar <114794086+Krishna2080@users.noreply.github.com>
2025-05-21 18:11:39 +0530

    Update Assignment5_6.py

--- Assignment_5/Assignment5_6.py
+++ Assignment_5/Assignment5_6.py
@@ -1,3 +1,4 @@
+#Q6. Celsius to Fahrenheit Converter Accept temperature in Celsius and convert it to Fahrenheit using the formula: F= (C x 9/5) + 32 
 def Fahrenheit(v1):
     F=(v1*9/5)+32
     print("Temprature in Fahrenheit : ",F)
