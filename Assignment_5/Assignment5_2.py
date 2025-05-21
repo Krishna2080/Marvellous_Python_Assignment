54519bb98cbe7bf2a09f1d772482e9438497ee24
Padmanabh Madaswar <114794086+Krishna2080@users.noreply.github.com>
2025-05-21 18:07:40 +0530

    Update Assignment5_2.py

--- Assignment_5/Assignment5_2.py
+++ Assignment_5/Assignment5_2.py
@@ -1,3 +1,4 @@
+#Q2. Vowel or Consonant Check Accept a single character from the user and check if it is a vowel (a, e, i, o, u). If not, print it's a consonant. 
 def vowel(v1):
     if(v1=="a") or (v1=="e") or (v1=="i") or (v1=="o") or (v1=="u"):
         print(v1,"is a Vowel")
