90dfe692139cde5f7e5df619f84b0cf98e2bdd5d
Padmanabh Madaswar <114794086+Krishna2080@users.noreply.github.com>
2025-05-29 21:59:13 +0530

    Add files via upload

--- Assignment_9/Assignment9_1.py
+++ Assignment_9/Assignment9_1.py
@@ -1,1 +1,22 @@
+import time
+import threading
 
+def thread():
+    for i in range(1,6):
+        print(i)
+        time.sleep(1)
+
+def main():
+    t1=threading.Thread(target=thread)
+    t1.start()
+    t1.join()
+    t2=threading.Thread(target=thread)
+    t2.start()
+    t2.join()
+    t3=threading.Thread(target=thread)
+    t3.start()
+    t3.join()
+
+
+if __name__=="__main__":
+    main()
