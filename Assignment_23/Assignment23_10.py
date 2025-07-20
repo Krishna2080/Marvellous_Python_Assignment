c972c74748ff70abab0ce5e86200ff90e6e015e6
Padmanabh Madaswar <114794086+Krishna2080@users.noreply.github.com>
2025-07-20 20:39:56 +0530

    Add files via upload

--- Assignment_23/Assignment23_10.py
+++ Assignment_23/Assignment23_10.py
@@ -8,9 +8,10 @@ def main():
         'English': [75, 85, 82]
     }
       df=pd.DataFrame(data)
-      print("Shape :",df.shape)
-      print("columns of data :",df.columns)
-      print("Data types of data :",df.dtypes)
+      print(df)
+      df.drop(columns=['English'] ,inplace=True)
+      print(df)
 
+      
 if __name__=="__main__":
     main()
